dda045f858600fca8424ccdd7111c85d9cdea6a8
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-16 13:53:46 -0400

    Updated from Brython Server: 9/16/2016 1:53:58 PM

--- birthday.py
+++ birthday.py
@@ -44,12 +44,12 @@ todaydate = datetime.today().day
 if year in [1980, 1981, 1982, 1983, 1984,1985, 1986, 1987, 1988, 1989]:
     gen = eighties
 else:
-if year in [1990, 1991, 1992, 1993, 1994,1995, 1996, 1997, 1998, 1999]:
-    gen = nineties
-else:
-if year >= 2000
-    gen = oughts
-else: 
-    gen = stoneage
+    if year in [1990, 1991, 1992, 1993, 1994,1995, 1996, 1997, 1998, 1999]:
+        gen = nineties
+    else:
+        if year >= 2000:
+            gen = oughts
+        else: 
+            gen = stoneage
 if month in ["December", "January" "February:"]:
     
