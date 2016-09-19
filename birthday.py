8179f289819cee075b6252d43c9da516bf307976
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-19 13:47:18 -0400

    Updated from Brython Server: 9/19/2016 1:47:19 PM

--- birthday.py
+++ birthday.py
@@ -59,6 +59,9 @@ else:
                     if month=="June"
                         monthnum=6
                     else:
+                        if month=="July"
+                            monthnum=7
+                        else:
 
 if int(year) in [1980, 1981, 1982, 1983, 1984,1985, 1986, 1987, 1988, 1989]:
     gen = "eighties"
