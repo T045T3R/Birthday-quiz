89534c09b9c5c811017191489366396f25b851fb
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-19 13:40:51 -0400

    Updated from Brython Server: 9/19/2016 1:40:52 PM

--- birthday.py
+++ birthday.py
@@ -67,4 +67,7 @@ else:
 if month == "October" and int(day) == 31:
     print("You were born on Halloween!")
 else:
-    print(name+", you are a "+season+" baby of the "+gen+".")
+    if todaymonth == month and int(day) == todaydate:
+        print("Happy birthday!")
+    else:
+     print(name+", you are a "+season+" baby of the "+gen+".")
