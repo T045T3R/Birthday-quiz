57465c5e2252a5ba244a514b96f5c64401ad8925
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-19 13:43:38 -0400

    Updated from Brython Server: 9/19/2016 1:43:39 PM

--- birthday.py
+++ birthday.py
@@ -41,8 +41,6 @@ from calendar import month_name
 todaymonth = datetime.today().month
 todaydate = datetime.today().day
 
-print(todaymonth)
-print(todaydate)
 
 if int(year) in [1980, 1981, 1982, 1983, 1984,1985, 1986, 1987, 1988, 1989]:
     gen = "eighties"
